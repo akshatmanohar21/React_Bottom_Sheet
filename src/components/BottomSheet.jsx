eead18fc32ff3835468c774139b968cac7538a14
Akshat Manohar <51690209+akshatmanohar21@users.noreply.github.com>
2025-07-12 23:16:16 +0530

    skeleton commit

---
+++ src/components/BottomSheet.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function BottomSheet() {
+    return (
+        <div className="bottom-sheet">
+        <div className="handle"></div>
+        <div className="content">
+          <h2>Bottom Sheet</h2>
+          <p>This is the initial skeleton of the bottom sheet component.</p>
+        </div>
+      </div>
+    );
+}
